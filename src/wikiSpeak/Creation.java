58b025e9da7a2025683de5cd3785e33db5b5f2df
Andonoo <andrew.donovan12@outlook.co.nz>
2019-09-28 16:35:54 +1200

    Commiting prior to new rm -r call

--- src/wikiSpeak/Creation.java
+++ src/wikiSpeak/Creation.java
@@ -59,7 +59,7 @@ public class Creation extends Playable{
 		if (result.get() == buttonTypeYes){
 				Thread worker = new Thread(()->{
 				try {
-					String command = String.format("rm %s", _filePath);
+					String command = String.format("rm -r %s", _directoryPath);
 					ShellHelper.execute(command);
 					// Run the action specified by client, mainly for refreshing the UI
 					Platform.runLater(()-> _afterDelete.run());
