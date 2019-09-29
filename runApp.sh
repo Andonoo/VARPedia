74449a1c1fe86e6edaebd6edfb5d7f38485c255f
Andonoo <andrew.donovan12@outlook.co.nz>
2019-09-29 21:06:15 +1300

    Added script file for running app

---
+++ runApp.sh
@@ -0,0 +1,2 @@
+/home/student/Downloads/openjdk-13_linux-x64_bin/jdk-13/bin/java --module-path /home/student/Downloads/openjfx-13-rc+2_linux-x64_bin-sdk/javafx-sdk-13/lib --add-modules javafx.controls,javafx.fxml,javafx.media -cp bin:openjfx-13-rc+2_linux-x64_bin-sdk/javafx-sdk-13/lib:libs:. wikiSpeak.Main
+
