efbd26c296f85b25e42253df234a3a7a45c02fb4
Andonoo <andrew.donovan12@outlook.co.nz>
2019-10-22 15:04:27 +1300

    - Added functionality to track the player's score for the current   game as well as their all-time score. - Added scoreboard at the end of the quiz to display to the user   how they did. - Added method in Main which is called on the closing of the application.   Here we should put all the system calls to clean up any leftover files   in the case that the user closes the application mid-activity.

--- src/wikiSpeak/Main.java
+++ src/wikiSpeak/Main.java
@@ -1,8 +1,10 @@
 package wikiSpeak;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import wikiSpeakController.SceneSwitcher;
 import wikiSpeakController.ShellHelper;
 import wikiSpeakController.SceneSwitcher.SceneOption;
@@ -18,6 +20,23 @@ public class Main extends Application{
 		Scene scene = new Scene(SceneSwitcher.getLayout(SceneOption.Main));
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		    @Override
+		    public void handle(WindowEvent event) {
+		    	onClose();
+		    }
+		});
+	}
+	
+	private void onClose() {
+		try {
+			String command = "rm -r .Game/.Round*";
+			ShellHelper.execute(command);
+			stop();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String args[]) {
@@ -35,6 +54,7 @@ public class Main extends Application{
 		}
 		
 		launch(args);
+		
 	}
 	
 	public static void deleteAllCreations() {
--- src/wikiSpeakModel/GameRecord.java
+++ src/wikiSpeakModel/GameRecord.java
@@ -1,6 +1,13 @@
 package wikiSpeakModel;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 /**
  * Data structure used to store the record for any player.
@@ -9,25 +16,84 @@ import java.io.Serializable;
  *
  */
 public class GameRecord implements Serializable {
+	private int _round;
+	private String _player;
+	private List<PlayerGuess> _guessesThisGame;
+	private int _totalScore;
+	private int _netScoreThisRound;
 	private int _correctGuesses;
-	private int _wrongGuesses;
-	private int _totalRoundsPlayed;
+	private int _incorrectGuesses;
+	private int _roundsPlayed;
 	
-	public GameRecord() {
+	public GameRecord(String playerName) {
+		_round = 0;
 		_correctGuesses = 0;
-		_wrongGuesses = 0;
-		_totalRoundsPlayed = 0;
+		_incorrectGuesses = 0;
+		_roundsPlayed = 0;
+		_netScoreThisRound = 0;
+		_totalScore = 0;
+		_player = playerName;
+		_guessesThisGame = new ArrayList<PlayerGuess>();
 	}
 	
-	public void incrementCorrect() {
-		_correctGuesses++;
+	/**
+	 * Adds a guess to the record.
+	 * @param guess the players's answer
+	 * @param answer the correct answer
+	 * @param correct whether their guess was correct
+	 */
+	public void addGuess(String guess, String answer, boolean correct) {
+		_guessesThisGame.add(new PlayerGuess(guess, answer, _round, correct));
+		if (correct) {
+			_correctGuesses ++;
+			_totalScore++ ;
+			_netScoreThisRound++;
+		} else {
+			_incorrectGuesses ++;
+			_totalScore--;
+			_netScoreThisRound--;
+		}
+		_roundsPlayed++;
 	}
 	
-	public void incrementWrong() {
-		_wrongGuesses++;
-	}
-
+	/**
+	 * Increment the round for recording purposes.
+	 */
 	public void incrementRounds() {
-		_totalRoundsPlayed++;
+		_round++;
+	}
+	
+	/**
+	 * Returns a list of guesses for this current game.
+	 * @return
+	 */
+	public List<PlayerGuess> getGuesses() {
+		return _guessesThisGame;
+	}
+	
+	/**
+	 * Returns the players score from all time.
+	 * @return
+	 */
+	public int getTotalScore() {
+		return _totalScore;
+	}
+	
+	/**
+	 * Returns the players net score from this game.
+	 * @return
+	 */
+	public int _getScoreThisRound() {
+		return _netScoreThisRound;
+	}
+	
+	/**
+	 * Perform end of game cleanup (deleting guesses so that they will not be shown during the next 
+	 * game for this player).
+	 */
+	public void endOfGame() {
+		_guessesThisGame = new ArrayList<PlayerGuess>();
+		_round = 0;
+		_netScoreThisRound = 0;
 	}
 }
