be933f811cbdd8cd58ff4f96cc58669fe569668e
Andonoo <andrew.donovan12@outlook.co.nz>
2019-10-24 19:10:43 +1300

    Added video player sliders. Combined play and pause buttons.

--- src/wikiSpeakController/FinalizeCreationController.java
+++ src/wikiSpeakController/FinalizeCreationController.java
@@ -152,7 +152,6 @@ public class FinalizeCreationController {
 				String path = item.getPath();
 				selectedImageString.add(path.substring(path.lastIndexOf('/')+1));
 			}
-			
 
 			String creationDir = "Creations/" + _creationName + "/";
 			try {
